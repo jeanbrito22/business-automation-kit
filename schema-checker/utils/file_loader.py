683f4e216bf70ffb93104cf9c35bbabab9cfd88e
Jean Brito <jeanbrito22@gmail.com>
2025-05-29 15:34:37 -0300

    refactor(header): torna limpeza de nomes de colunas mais legível e robusta

--- schema-checker/utils/file_loader.py
+++ schema-checker/utils/file_loader.py
@@ -11,10 +11,14 @@ def load_csv(path, schema_path=None, delimiter=","):
     expected_fields = [col["source_column"] for col in load_schema(schema_path)["table_spec"][0]["schema"]] if schema_path else []
 
     def normalize_header(h):
-        h = h.strip().strip('"')  # remove espaços e aspas duplas do início/fim
-        cleaned = " ".join(h.split()).replace("°", "º").replace("˚", "º")
-        return cleaned if cleaned not in expected_fields else next(
-            (s for s in expected_fields if unidecode(s) == unidecode(cleaned)), cleaned)
+        h = h.strip()                    # remove espaços no início/fim
+        h = h.strip('"').strip("'")      # remove aspas duplas e simples se estiverem nas pontas
+        h = h.replace("°", "º").replace("˚", "º")
+        h = " ".join(h.split())          # remove espaços duplicados
+        return next(
+            (s for s in expected_fields if unidecode(s) == unidecode(h)),
+            h
+        )
 
 
     with open(path, newline='', encoding='utf-8-sig') as f:
